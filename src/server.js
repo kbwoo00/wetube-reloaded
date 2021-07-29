cd6f7bd8d7fd65f04f982cd09a6318def9c6029b
Byung Woo <kbwoo1065@gmail.com>
2021-07-29 16:24:26 +0900

    깃허브 로그인까지

--- src/server.js
+++ src/server.js
@@ -1,18 +1,39 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
-
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET, // 쿠키에 sign할 때 쓰는 암호같은거?
+    resave: false,
+    saveUninitialized: false, // 로그인한 유저들만 세션을 받게 해줌
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
+// app.use((req, res, next) => {
+//   req.sessionStore.all((error, sessions) => {
+//     console.log(sessions);
+//     next();
+//   });
+// });
+
+app.use(localsMiddleware);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 export default app;
+//jGY4i0Jqnn8kOz9SZHtw-ZIfOXtUGwkm
